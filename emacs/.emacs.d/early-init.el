93bb98f5a88725fb6cd306fff738a36047ea1571
Lao Tong <lao.s.t@pm.me>
2022-07-12 12:35:15 -0600

    configure for faster bootup times

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,14 +1,37 @@
-;;; early-init.el --- Early initialization. -*- lexical-binding: t -*-
-(setq gc-cons-threshold 1024 * 1024 * 1024)
+;;; early-init.el -*- lexical-binding: t -*-
+(setq package-enable-at-startup nil)
+
+(setq native-comp-deferred-compilation nil)
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 16 1024 1024)
+                  gc-cons-percentage 0.1)))
 
 (setq frame-inhibit-implied-resize t)
+
+(setq load-prefer-newer noninteractive)
+
+(setq-default inhibit-message t)
+(add-hook 'window-setup-hook
+          (lambda ()
+            (setq-default inhibit-redisplay nil
+                          inhibit-message nil)
+            (redisplay)))
+
+;; suppress messages from `load-file'
+(define-advice load-file (:override (file) silence)
+  (load file nil 'nomessage))
+
+(set-language-environment "UTF-8")
+(setq default-input-method nil)
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 (push '(fullscreen . fullheight) default-frame-alist)
 (push '(fullscreen . maximized) default-frame-alist)
-
-(setq package-enable-at-startup nil)
 
 (provide 'early-init)
